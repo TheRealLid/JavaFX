513d4653b97dcdf8850d125a98c58306089104ec
Matthew Q <matthewrileyquinn@gmail.com>
2024-09-09 17:33:08 -0700

    Created Task Class to simplify project moving forward.

---
+++ src/Task.java
@@ -0,0 +1,39 @@
+
+public class Task {
+	private String text;
+	private Boolean isCompleted;
+	private int priority;
+	
+	public Task(String text) {
+		this.text = text;
+		this.isCompleted = false;
+	}
+	
+	public String getText() {
+		return text;
+	}
+	
+	public void setText(String text) {
+		this.text = text;
+	}
+	
+	public Boolean getIsCompleted() {
+		return isCompleted;
+	}
+	
+	public int getpriority() {
+		return priority;
+	}
+	
+	public void setPriority(int priority) {
+		this.priority = priority;
+	}
+	
+	public void setIsCompleted(Boolean isCompleted) {
+		this.isCompleted = isCompleted;
+	}
+	
+	public void completeTask() {
+		this.isCompleted = !this.isCompleted;
+	}
+}
